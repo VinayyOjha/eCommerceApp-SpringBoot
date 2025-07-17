be042dfe6f8f4e69423200c358a60de27d6a39c3
VinayyOjha <ojhavinay7602@.com>
2025-07-17 06:13:00 +0530

    implemented @Component annotation

--- eCommerce/src/main/java/com/example/eCommerce/ECommerceApplication.java
+++ eCommerce/src/main/java/com/example/eCommerce/ECommerceApplication.java
@@ -2,12 +2,18 @@ package com.example.eCommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
+		
+		ApplicationContext context =  SpringApplication.run(ECommerceApplication.class, args);
+
+		Dev obj = context.getBean(Dev.class);
+		obj.build();
 	}
 
 }
+ 
