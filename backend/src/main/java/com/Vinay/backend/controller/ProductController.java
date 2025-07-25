7d3b30c19ec1f4e3003252fed12825617604365d
VinayyOjha <ojhavinay7602@.com>
2025-07-25 07:58:41 +0530

    getProductById() functiinality added

--- backend/src/main/java/com/Vinay/backend/controller/ProductController.java
+++ backend/src/main/java/com/Vinay/backend/controller/ProductController.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.Vinay.backend.model.Product;
 import com.Vinay.backend.service.ProductService;
@@ -20,7 +19,7 @@ import com.Vinay.backend.service.ProductService;
 public class ProductController {
 
     @Autowired
-    private ProductService service;
+    private ProductService service; // Service you are responsible to get me product.
 
     @RequestMapping("/")
     public String greet() {
@@ -30,5 +29,10 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getAllProducts() {
         return service.getAllProducts();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable int id) {
+        return service.getProductById(id);
     }
 }
--- backend/src/main/java/com/Vinay/backend/service/ProductService.java
+++ backend/src/main/java/com/Vinay/backend/service/ProductService.java
@@ -11,25 +11,26 @@ import com.Vinay.backend.repository.ProductRepo;
 
 @Service
 public class ProductService {
+
     private final ProductRepo repo;
 
     public ProductService(ProductRepo repo) {
         this.repo = repo;
     }
 
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).get();
     }
 }
 
 // ✔ Benefits of using final:
 // Thread safety: Once initialized, the reference can’t change — no accidental reassignment.
-
 // Clarity: Makes it obvious this field is a required dependency.
-
 // Constructor enforcement: Ensures that the dependency is initialized when the object is created.
-
 // Easier to reason about: You can be confident that repo will never be null after construction.
-
 // Good practice in dependency injection: Especially with constructor injection (recommended over @Autowired on fields).
 
